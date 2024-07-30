0965ed173d226e0614187074f7abe8af855bb0b3
Amanda2428 <maymyatthu2428@gmail.com>
2024-07-31 02:09:09 +0630

    register add

--- registration.php
+++ registration.php
@@ -119,16 +119,7 @@ if (isset($_POST['btnReg'])) {
       <section>
   </main>
 
-  <footer>
-    <p>You are here: Registration</p>
-    <div class="footer-content">
-      <p>&copy; 2024 Online Safety Campaign</p>
-      <!-- Add social media buttons with relevant links -->
-      <a href="#" style="color: white">Facebook</a>
-      <a href="#" style="color: white; margin-left: 10px">Twitter</a>
-      <a href="#" style="color: white; margin-left: 10px">Instagram</a>
-    </div>
-  </footer>
+  <?php include("defaultfooter.php") ?>
   <!-- Bootstrap 5 JavaScript -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
   <!-- Link to the external JavaScript file -->
